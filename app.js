4ab17729935a88554b1fb896881823e3e4d33d83
Scott Schipke <swschipke@gmail.com>
2020-04-22 22:20:40 -0600

    Release working server to master (#4)
    * Update requiring of modules
    
    * Add server validation for login and register (#2)
    
    * Create user schema (#1)
    
    * Merge working server into develop (#3)
    
    * Create user schema (#1)
    
    * Add route for registering
    
    * Configure passport.js
    
    * Add route for login
    
    * Update app to use passport and user routes
    
    * Correct type in users route
    
    * Update error for mismatching passwords

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
 
 const app = express();
 
@@ -13,14 +16,22 @@ app.use(
 app.use(bodyParser.json());
 
 //db config 
-const db = proccess.env.MONGO_URI;
+const db =require("./config/keys").mongoURI;
 
-console.log(process.env.TEST_VAR)
+console.log('Testing! --->',require("./config/keys").test)
 
 mongoose.connect(
   db,
-  {useNewUrlParser: true}
+  { useNewUrlParser: true, useUnifiedTopology: true  }
 )
 .then(() => console.log('Mongo successfully connected!'))
 .catch(err => console.error(err));
 
+app.use(passport.initialize());
+
+require("./config/passport")(passport);
+
+app.use("/api/users", users);
+
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 
-import app from './app';
+const app = require('./app')
 
 app.set('port', process.env.PORT || 5050);
 
 app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is running on localhost:${app.get('port')}`);
+  console.log(`MERN AUTH is running on localhost:${app.get('port')}`);
 });
