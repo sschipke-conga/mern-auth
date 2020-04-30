908d780d1bbdacfcac9fcee84693314b52572188
Scott Schipke <swschipke@gmail.com>
2020-04-29 21:32:44 -0600

    Integrating redux (#10)
    * Add thunks to Register component
    
    * Add loginUser action to Login form
    
    * Creat Dashboard and PrivateRoute components
    
    * Add new components to App
    
    * Fix proxy issue in package.json
    
    * Remove componentWillReceiveProps from Register.js
    
    * Finish redux changes

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -10,18 +10,18 @@ import {
 
 export const registerUser = (userData, history) => dispatch => {
   axios
-    .post('api/users/register', userData)
-    .then(res => history.push('/login')) //redirect to login
-    .catch(err => 
+    .post("/api/users/register", userData)
+    .then((res) => history.push("/login")) //redirect to login
+    .catch((err) =>
       dispatch({
         type: GET_ERRORS,
-        payload: err.response.data
+        payload: err.response.data,
       })
     );
 };
 
 export const loginUser = userData => dispatch => {
-  axios.post('api/users/login', userData)
+  axios.post('/api/users/login', userData)
   .then(res => {
     const { token } = res.data;
     localStorage.setItem("jwtToken", token);
@@ -37,7 +37,7 @@ export const loginUser = userData => dispatch => {
   );
 };
 
-export const setCurrentUser = decoded => {
+export const setCurrentUser = decodedToken => {
   return {
     type: SET_CURRENT_USER,
     payload: decodedToken
