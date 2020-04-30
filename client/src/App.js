f44f3b155db813f4e54027c485f4866b65f0d3e8
Scott Schipke <swschipke@gmail.com>
2020-04-29 21:42:04 -0600

    Complete MVP (#12)
    * Update requiring of modules
    
    * Add server validation for login and register (#2)
    
    * Create user schema (#1)
    
    * Merge working server into develop (#3)
    
    * Create user schema (#1)
    
    * Add route for registering
    
    * Configure passport.js
    
    * Add route for login
    
    * Update app to use passport and user routes
    
    * Correct type in users route
    
    * Update error for mismatching passwords
    
    * Create React app to begin front end development (#5)
    
    * Install React for frontend
    
    * Install react deps
    
    * Add material ui to html
    
    * Touch Landing and Navbar components
    
    * Add material ui to html (#6)
    
    * Add components to login and register
    
    * Fix merge conflicts (#7)
    
    * Adding redux (#8)
    
    * Setup redux store
    
    * Setup initial state for auth and error reducers
    
    * Set auth token to axios request headers
    
    * Add authActions + add rootReducer to initialState
    
    * Integrating redux (#10)
    
    * Add thunks to Register component
    
    * Add loginUser action to Login form
    
    * Creat Dashboard and PrivateRoute components
    
    * Add new components to App
    
    * Fix proxy issue in package.json
    
    * Remove componentWillReceiveProps from Register.js
    
    * Finish redux changes

---
+++ client/src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { Provider } from 'react-redux';
+import store from './store';
+import './App.css';
+
+import { setCurrentUser, logoutUser } from './actions/authActions';
+
+import Landing from './components/layout/Landing';
+import Navbar from './components/layout/Navbar';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import PrivateRoute from './components/private-route/PrivateRoute';
+import Dashboard from './components/dashboard/Dashboard';
+
+if (localStorage.jwtToken) {
+  const token = localStorage.jwtToken
+  setAuthToken(token);
+  const decodedToken = jwt_decode(token);
+  store.dispatch(setCurrentUser(decodedToken));
+
+  const setCurrentTime = Date.now() / 1000;
+  if(decodedToken.exp < setCurrentTime) {
+    store.dispatch(logoutUser());
+    window.location.href = './login';
+  }
+}
+
+function App() {
+  return (
+    <Provider store={store} >
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Switch>
+            <PrivateRoute exact path="/dashboard" component={Dashboard} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
