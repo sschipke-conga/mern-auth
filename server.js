815136281125343eb2b2a973bad9cad7de5435d3
Scott Schipke <swschipke@gmail.com>
2020-05-01 16:00:14 -0600

    Merge deploy settings to master (#14)
    * Update requiring of modules
    
    * Add server validation for login and register (#2)
    
    * Create user schema (#1)
    
    * Merge working server into develop (#3)
    
    * Create user schema (#1)
    
    * Add route for registering
    
    * Configure passport.js
    
    * Add route for login
    
    * Update app to use passport and user routes
    
    * Correct type in users route
    
    * Update error for mismatching passwords
    
    * Create React app to begin front end development (#5)
    
    * Install React for frontend
    
    * Install react deps
    
    * Add material ui to html
    
    * Touch Landing and Navbar components
    
    * Add material ui to html (#6)
    
    * Add components to login and register
    
    * Fix merge conflicts (#7)
    
    * Adding redux (#8)
    
    * Setup redux store
    
    * Setup initial state for auth and error reducers
    
    * Set auth token to axios request headers
    
    * Add authActions + add rootReducer to initialState
    
    * Integrating redux (#10)
    
    * Add thunks to Register component
    
    * Add loginUser action to Login form
    
    * Creat Dashboard and PrivateRoute components
    
    * Add new components to App
    
    * Fix proxy issue in package.json
    
    * Remove componentWillReceiveProps from Register.js
    
    * Finish redux changes
    
    * Deploying (#13)
    
    * Add postbuild script
    
    * Convert congifs to env variables

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 
 const app = require('./app')
 
+if (process.env.NODE_ENV === "production") {
+  app.user(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.set('port', process.env.PORT || 5050);
 
 app.listen(app.get('port'), () => {
