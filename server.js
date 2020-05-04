7cf7bc7dfea203a4b9c5a1d7f0d7fe3c19cf438f
Scott <swschipke@gmail.com>
2020-05-04 14:26:34 -0600

    Update server to build directory

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV === "production") {
   app.user(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build/static", "index.html"));
   });
 }
 
