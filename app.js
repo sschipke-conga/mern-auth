07fe80d3eb498bb0d49de34a77d7784f4c62b4ac
Scott <swschipke@gmail.com>
2020-05-04 14:50:39 -0600

    Troubleshoot db

--- app.js
+++ app.js
@@ -27,7 +27,11 @@ if (process.env.NODE_ENV === "production") {
 }
 
 //db config 
-const db =process.env.MONGO_URI
+const db = `${process.env.MONGO_URI}`
+
+console.log(db)
+
+
 
 mongoose.connect(
   db,
