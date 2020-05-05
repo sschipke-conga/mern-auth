0100a41cc2fb525b13b712cbcd870d4df15ba732
Scott <swschipke@gmail.com>
2020-05-04 21:45:37 -0600

    update passport

--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,7 @@ const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
-const keys = `${process.env.SECRET_OR_KEY}`;
+const keys = process.env.SECRET_OR_KEY;
 const opts = {};
 
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
