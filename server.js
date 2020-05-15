8bc3967f35ba7449f67cb734f6c1e75835bf311e
Scott <swschipke@gmail.com>
2020-05-14 23:07:20 -0600

    Remove extra logic

--- server.js
+++ server.js
@@ -1,13 +1,4 @@
-const express = require("express");
 const app = require('./app')
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 
 app.set('port', process.env.PORT || 5050);
 
